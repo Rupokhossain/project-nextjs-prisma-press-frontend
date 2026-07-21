cfbc3d5af2b03b8ce9576165076d3ed1c5385f35
Rupok Hossain Siam <rh.siam999@gmail.com>
2026-07-21 16:40:08 +0600

    Redirecting Users To Role Based Routes After Successful Login, Proxy File in Next.js For Authorization Of Routes, Implementation Of Proxy File In The Project, Implementation Of Proxy File In The Project, Catch All Routes In Matcher Of Proxy File, Handling Auth Routes In Proxy File

---
+++ proxy.ts
@@ -0,0 +1,68 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+const AUTH_ROUTES = ["/login", "/register"];
+const PUBLIC_ROUTES = ["/", "/news"];
+
+// This function can be marked `async` if using `await` inside
+export async function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  console.log(pathname, "pathname");
+
+  //   const cookieStore = await cookies();
+  //   const accessToken = cookieStore.get("accessToken")?.value
+
+  const accessToken = request.cookies.get("accessToken")?.value;
+
+  const decodedToken = accessToken
+    ? (jwt.decode(accessToken) as JwtPayload)
+    : null;
+
+  let userRole = null;
+
+  if (decodedToken) {
+    userRole = decodedToken.role;
+  }
+
+  // user is logged in and trying to access login or register page. redirect to dashboard or root home page
+
+  if (accessToken && AUTH_ROUTES.includes(pathname)) {
+    if (userRole === "USER") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    } else if (userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    } else if (userRole === "AUTHOR") {
+      return NextResponse.redirect(new URL("/author-dashboard", request.url));
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
+  const isPublicRoute = PUBLIC_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+
+  const isAuthRoute = AUTH_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+
+  // Authenticaiton pages protection: authoraization is not handle yet
+
+  if (!accessToken && !isPublicRoute && !isAuthRoute) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  //   return NextResponse.redirect(new URL("/", request.url));
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    // "/dashboard/:path*",
+    // "/admin-dashboard/:path*"
+
+    "/((?!api|_next/static|_next/image|.*\\.png$).*)",
+  ],
+};
